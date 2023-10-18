86f48357c59b3f78797416bde0b1c9c3d6d52831
Kgomotso <kgmodjadji@gmail.com>
2023-10-18 11:05:47 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,179 @@
+/////////////////////////////////////////////////////////////////////////
+///// IMPORT
+import './main.css'
+import * as THREE from 'three'
+import * as CANNON from 'cannon-es';
+import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import CannonDebugger from 'cannon-es-debugger';
+
+/////////////////////////////////////////////////////////////////////////
+//// DRACO LOADER TO LOAD DRACO COMPRESSED MODELS FROM BLENDER
+const dracoLoader = new DRACOLoader()
+const loader = new GLTFLoader()
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/')
+dracoLoader.setDecoderConfig({ type: 'js' })
+loader.setDRACOLoader(dracoLoader)
+
+/////////////////////////////////////////////////////////////////////////
+///// DIV CONTAINER CREATION TO HOLD THREEJS EXPERIENCE
+const container = document.createElement('div')
+document.body.appendChild(container)
+
+/////////////////////////////////////////////////////////////////////////
+///// SCENE CREATION
+const scene = new THREE.Scene()
+scene.background = new THREE.Color('#c8f0f9')
+
+
+/////////////////////////////////////////////////////////////////////////
+///// RENDERER CONFIG
+const renderer = new THREE.WebGLRenderer({ antialias: true}) // turn on antialias
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)) //set pixel ratio
+renderer.setSize(window.innerWidth, window.innerHeight) // make it full screen
+renderer.outputEncoding = THREE.sRGBEncoding // set color encoding
+container.appendChild(renderer.domElement) // add the renderer to html div
+
+/////////////////////////////////////////////////////////////////////////
+///// CAMERAS CONFIG
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 2000 );
+scene.add(camera)
+camera.position.set(0,10,4); // Set position like this
+camera.lookAt(new THREE.Vector3(0,0,0)); // Set look at coordinate like this
+
+var dice
+
+
+/////////////////////////////////////////////////////////////////////////
+///// MAKE EXPERIENCE FULL SCREEN
+window.addEventListener('resize', () => {
+    const width = window.innerWidth
+    const height = window.innerHeight
+    camera.aspect = width / height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(width, height)
+    renderer.setPixelRatio(2)
+})
+
+/////////////////////////////////////////////////////////////////////////
+///// CREATE ORBIT CONTROLS
+const controls = new OrbitControls(camera, renderer.domElement)
+
+/////////////////////////////////////////////////////////////////////////
+///// SCENE LIGHTS
+const ambient = new THREE.AmbientLight(0xa0a0fc, 0.82)
+scene.add(ambient)
+
+const sunLight = new THREE.DirectionalLight(0xe8c37b, 1.96)
+sunLight.position.set(-69,44,14)
+scene.add(sunLight)
+
+/////////////////////////////////////////////////////////////////////////
+///// LOADING GLB/GLTF MODEL FROM BLENDER
+loader.load( 'models/gltf/dice.glb', function ( gltf ) {
+
+	dice =  gltf.scene;
+  scene.add(dice);
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+
+/////////////////////////////////////////////////////////////////////////
+//// DEFINE ORBIT CONTROLS LIMITS
+function setOrbitControlsLimits(){
+    controls.enableDamping = true
+    controls.dampingFactor = 0.04
+    controls.minDistance = 0
+    controls.maxDistance = 500
+    controls.enableRotate = true
+    controls.enableZoom = true
+    //controls.maxPolarAngle = Math.PI /2.5
+}
+
+setOrbitControlsLimits()
+
+/////////////////////////////////////////////////////////////////////////
+//// RENDER LOOP FUNCTION
+function rendeLoop() {
+
+    TWEEN.update() // update animations
+
+    controls.update() // update orbit controls
+
+    renderer.render(scene, camera) // render the scene using the camera
+
+    requestAnimationFrame(rendeLoop) //loop the render function
+    
+}
+
+rendeLoop() //start rendering
+
+/////////////////////////////////////////////////////////////////////////
+///// INITIALIZE PHYSICS
+const groundPhysMat = new CANNON.Material();
+
+const physicsWorld = new CANNON.World({
+  gravity: new CANNON.Vec3(0, -9.82, 0),
+});
+
+const groundBody = new CANNON.Body({
+  type: CANNON.Body.STATIC,
+  shape: new CANNON.Plane(),
+  material: groundPhysMat
+})
+
+groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+physicsWorld.addBody(groundBody)
+
+const boxPhysMat = new CANNON.Material();
+const diceBody = new CANNON.Body({
+  mass: 1,
+  shape: new CANNON.Box(new CANNON.Vec3(1,1,1)),
+  material: boxPhysMat
+});
+
+diceBody.position.set(0, 8, 0);
+physicsWorld.addBody(diceBody);
+
+diceBody.angularVelocity.set(10, 10, 0 );
+diceBody.angularDamping = 0.5;
+
+const groundBoxContactMat = new CANNON.ContactMaterial(
+  groundPhysMat,
+  boxPhysMat,
+  {friction: 0.3,restitution:0.4}
+);
+
+physicsWorld.addContactMaterial(groundBoxContactMat);
+
+const cannonDebugger = new CannonDebugger(scene.scene, physicsWorld, {
+  //color: 0xff0000,
+});
+
+function animate() {
+  physicsWorld.fixedStep();
+  
+	requestAnimationFrame( animate );
+  if (diceBody.position != undefined && dice.position.copy != undefined){
+    dice.position.copy(diceBody.position);
+  }
+  console.log(dice.position);
+
+  
+}
+
+animate();
+
+/*Event Listeners*/
+window.addEventListener("resize", ()=>{
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
